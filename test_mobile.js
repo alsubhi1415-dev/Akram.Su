6311b496e90f52fe85e1ac5e7632017349b11238
alsubhi1415-dev <alsubhi1415@gmail.com>
2026-07-30 23:35:24 +0300

    الإصدار 14.3: درع بطاقة الجاهزية وجرس التنبيهات الذهبي

--- test_mobile.js
+++ test_mobile.js
@@ -23,7 +23,7 @@ const click = async (label, ms) => {
   const D = w.document;
 
   ok("تمّ الإقلاع", D.getElementById("root").childNodes.length > 0);
-  ok("ختم الإصدار 14.2", D.getElementById("root").textContent.includes("14.2"));
+  ok("ختم الإصدار 14.3", D.getElementById("root").textContent.includes("14.3"));
 
   // --- سطح المكتب: الجدول هو الأصل ---
   setW(1280); await wait(400);
